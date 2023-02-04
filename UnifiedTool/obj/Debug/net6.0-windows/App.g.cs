da14f77591551900fa54b40de9ee9fb8dc34bdfd
yjn879 <2360331871@qq.com>
2023-02-04 13:38:08 +0800

    no commit message

--- UnifiedTool/obj/Debug/net6.0-windows/App.g.cs
+++ UnifiedTool/obj/Debug/net6.0-windows/App.g.cs
@@ -48,7 +48,7 @@ namespace UnifiedTool {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -74,7 +74,7 @@ namespace UnifiedTool {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         public static void Main() {
             UnifiedTool.App app = new UnifiedTool.App();
             app.InitializeComponent();
--- UnifiedTool/obj/Debug/net6.0-windows/App.g.i.cs
+++ UnifiedTool/obj/Debug/net6.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace UnifiedTool {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace UnifiedTool {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/UnifiedTool;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UnifiedTool;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace UnifiedTool {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         public static void Main() {
             UnifiedTool.App app = new UnifiedTool.App();
             app.InitializeComponent();
